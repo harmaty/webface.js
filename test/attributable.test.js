c7e5a95e75b21f05e042d498f49d742cfa34ba5e
Roman Snitko <roman.snitko@gmail.com>
2018-08-27 00:14:38 +0700

    Initial commit: adding Attributable class

---
+++ test/attributable.test.js
@@ -0,0 +1,90 @@
+import { Attributable, UndefinedAttributeError } from './lib/attributable.js'
+
+class Dummy extends Attributable {
+  constructor() {
+    super();
+    this.attribute_names = ["caption", "title", "attr3"];
+    this.default_attribute_values = { "attr3": "default_value" };
+  }
+}
+
+describe('Attributable', function() {
+
+  var dummy;
+  var spy;
+
+  beforeEach(function() {
+    dummy = new Dummy();
+  });
+
+  it('returns an attribute value when a getter is called', function() {
+    dummy.set("caption", "New Caption");
+    chai.expect(dummy.get("caption")).to.equal("New Caption");
+  });
+
+  it('returns an attribute value when a getter is called', function() {
+    chai.expect(function() { dummy.get("non_existent_attr") }).to.throw(UndefinedAttributeError);
+    chai.expect(function() { dummy.set("non_existent_attr", "x")}).to.throw(UndefinedAttributeError);
+  });
+
+  it('invokes a default callback if no custom callback for the attribute is defined', function() {
+    spy = chai.spy.on(dummy.attribute_callbacks, 'default');
+    dummy.set("title", 'New Title');
+    chai.expect(spy).to.have.been.called.with('title', dummy);
+  });
+
+  it('keeps previous value of the attribute', function() {
+    dummy.set('title', 'Title 1');
+    dummy.set('title', 'Title 2');
+    chai.expect(dummy.get('_old_title')).to.equal('Title 1');
+    dummy.set('title', 'Title 3');
+    chai.expect(dummy.get('_old_title')).to.equal('Title 2');
+  });
+
+  it('tells if new value is different from the old one', function() {
+    dummy.set('title', 'Title 1');
+    dummy.set('title', 'Title 2');
+    chai.expect(dummy.hasAttributeChanged("title")).to.be.true;
+    dummy.set('title', 'Title 2');
+    chai.expect(dummy.hasAttributeChanged("title")).to.be.false;
+  });
+
+  it('updates attributes in bulk', function() {
+    dummy.updateAttributes({"title" : "New Title", "caption": "New Caption"});
+    chai.expect(dummy.get("title")).to.equal("New Title");
+    chai.expect(dummy.get("caption")).to.equal("New Caption");
+  });
+
+  it('while updating attributes in bulk, ignores those with a dot', function() {
+    dummy.updateAttributes({"subcomponent.property" : "new value"})
+    // Doesn't throw UndefinedAttributeError
+  });
+
+  it('runs callbacks on attributes after updating them in bulk', function() {
+    dummy.attribute_callbacks["title"] = function() {}
+    spy = chai.spy.on(dummy.attribute_callbacks, 'title');
+    dummy.updateAttributes({"title" : "New Title"});
+    chai.expect(spy).to.have.been.called.with('title', dummy);
+  });
+
+  it('doesn\'t run callbacks on attributes after updating them in bulk if closure evaluates to false', function() {
+    dummy.attribute_callbacks["title"] = function() {}
+    dummy.updateAttributes({ 'caption' : 'new caption' }, function() { return false; });
+    chai.expect(spy).to.not.have.been.called.with('title', dummy);
+  });
+
+  it("sets default values for attributes", function() {
+    dummy.setDefaultAttributeValues();
+    chai.expect(dummy.get("attr3")).to.equal("default_value");
+  });
+
+  it("throws error if attribute doesn't exist and someone tries to update it with updateAttributes()", function() {
+    chai.expect(function() { dummy.updateAttributes({ 'non_existent_attr' : 'new caption' })}).to.throw(UndefinedAttributeError);
+  });
+
+  it("doesn't throw error if attribute doesn't exist but `ingore_non_existent: true` is passed to updateAttributes()", function() {
+    dummy.updateAttributes({ 'non_existent_attr' : 'new caption' }, { ignore_non_existent: true })
+    // Doesn't throw UndefinedAttributeError
+  });
+
+});
---
+++ test/webface.test.js
@@ -0,0 +1,1 @@
+import "./attributable.test.js";
