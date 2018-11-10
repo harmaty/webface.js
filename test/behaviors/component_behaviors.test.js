e06628d26c5f13d458bba3864526b7d3c0c565b3
Roman Snitko <roman.snitko@gmail.com>
2018-11-11 06:31:50 +0700

    ComponentBehaviors remaining unit tests

--- test/behaviors/component_behaviors.test.js
+++ test/behaviors/component_behaviors.test.js
@@ -27,16 +27,92 @@ describe("ComponentBehaviors", function() {
     dom_element.remove();
   });
 
-  it("hides component's dom_element", async function() {
-    await behaviors.hide();
-    chai.expect(dom_element.style.display).to.equal("none");
+  describe("showing/hiding", function() {
+    
+    it("hides component's dom_element", async function() {
+      await behaviors.hide();
+      chai.expect(dom_element.style.display).to.equal("none");
+    });
+
+    it("shows component's dom_element", async function() {
+      await behaviors.hide();
+      chai.expect(dom_element.style.display).to.equal("none");
+      await behaviors.show();
+      chai.expect(dom_element.style.display).to.equal("block")
+    });
+
+    it("toggles show/hide", async function() {
+      await behaviors.toggleDisplay();
+      chai.expect(dom_element.style.display).to.equal("none");
+      await behaviors.toggleDisplay();
+      chai.expect(dom_element.style.display).to.equal("block")
+    });
+    
   });
 
-  it("shows component's dom_element", async function() {
-    await behaviors.hide();
-    chai.expect(dom_element.style.display).to.equal("none");
-    await behaviors.show();
-    chai.expect(dom_element.style.display).to.equal("block")
+  describe("locking/unlocking", function() {
+    
+    it("locks the element by adding 'locked' as one of the css classes", function() {
+      behaviors.lock();
+      chai.expect(Array.from(dom_element.classList)).to.include("locked");
+    });
+
+    it("unlocks the element removing 'locked' from the list of css classes", function() {
+      dom_element.classList.add("locked");
+      behaviors.unlock();
+      chai.expect(Array.from(dom_element.classList)).to.not.include("locked");
+    });
+
+    it("toggles the lock/unlock behavior", function() {
+      behaviors.lock();
+      chai.expect(Array.from(dom_element.classList)).to.include("locked");
+      behaviors.unlock();
+      chai.expect(Array.from(dom_element.classList)).to.not.include("locked");
+    });
+
   });
+
+  describe("enabling/disabling", function() {
+    
+    it("disables the element by adding 'disabled' as one of the css classes and as an attribute", function() {
+      behaviors.disable();
+      chai.expect(Array.from(dom_element.classList)).to.include("disabled");
+      chai.expect(dom_element.hasAttribute("disabled")).to.be.true;
+    });
+
+    it("enables the element by removing 'disabled' as one of the css classes and as an attribute", function() {
+      dom_element.classList.add("disabled");
+      dom_element.setAttribute("disabled", "disabled")
+      behaviors.enable();
+      chai.expect(Array.from(dom_element.classList)).to.not.include("disabled");
+      chai.expect(dom_element.hasAttribute("disabled")).to.be.false;
+    });
+
+    it("toggles the disable/enable behavior", function() {
+      behaviors.toggleDisable();
+      chai.expect(Array.from(dom_element.classList)).to.include("disabled");
+      chai.expect(dom_element.hasAttribute("disabled")).to.be.true;
+      behaviors.toggleDisable();
+      chai.expect(Array.from(dom_element.classList)).to.not.include("disabled");
+      chai.expect(dom_element.hasAttribute("disabled")).to.be.false;
+    });
+
+  });
+
+  it("sets opacity to 0, but display to its original value so that we can calculate element dimensions, but not yet show it", function() {
+    behaviors.displayHidden();
+    chai.expect(dom_element.style.opacity).to.equal("0");
+    chai.expect(dom_element.style.display).to.equal("block");
+  });
+
+  it("switches block visibility on and/off", function() {
+    var block = new DOMParser().parseFromString('<div id="part"></div>', "text/html").documentElement.querySelector("body>div");
+    dom_element.append(block);
+    behaviors._switchBlockVisibilityIfExists("#part", "hide");
+    chai.expect(block.style.display).to.equal("none");
+    behaviors._switchBlockVisibilityIfExists("#part", "show");
+    chai.expect(block.style.display).to.equal("block");
+  });
+
 
 });
