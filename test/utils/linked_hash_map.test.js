fc30e1f4f3a221eefd6c82f3e22ac01ea0bbe6c4
Roman Snitko <roman.snitko@gmail.com>
2019-01-20 12:22:58 +0700

    LinkedHashMap class + unit tests

---
+++ test/utils/linked_hash_map.test.js
@@ -0,0 +1,92 @@
+import '../webface_init.js'
+import { LinkedHashMap } from '../lib/utils/linked_hash_map.js'
+
+describe("LinkedHashMap", function() {
+
+  var hash;
+
+  beforeEach(function() {
+    hash = new LinkedHashMap();
+  });
+
+  it("adds keys to the head", function() {
+    hash.addHead("key1", 1);
+    chai.expect(hash.head.value).to.equal(1);
+    chai.expect(hash.tail.value).to.equal(1);
+    hash.addHead("key2", 2);
+    chai.expect(hash.head.value).to.equal(2);
+    chai.expect(hash.tail.value).to.equal(1);
+    hash.addHead("key3", 3);
+    chai.expect(hash.head.value).to.equal(3);
+    chai.expect(hash.tail.value).to.equal(1);
+  });
+
+  it("adds keys to the tail", function() {
+    hash.addTail("key1", 1);
+    chai.expect(hash.head.value).to.equal(1);
+    chai.expect(hash.tail.value).to.equal(1);
+    hash.addTail("key2", 2);
+    chai.expect(hash.head.value).to.equal(1);
+    chai.expect(hash.tail.value).to.equal(2);
+    hash.addTail("key3", 3);
+    chai.expect(hash.head.value).to.equal(1);
+    chai.expect(hash.tail.value).to.equal(3);
+  });
+
+  describe("iterating", function() {
+
+    beforeEach(function() {
+      hash.addTail("key1", 1);
+      hash.addTail("key2", 2);
+      hash.addTail("key3", 3);
+    });
+
+    it("iterates over all the items with for-of", function() {
+      var result = [];
+      for(let [k,v] of hash)
+        result.push([k,v]);
+      chai.expect(result).to.eql([["key1", 1], ["key2", 2], ["key3", 3]]);
+    });
+
+
+    it("iterates over all the items with forEach() method", function() {
+      var result = [];
+      hash.forEach((k,v) => {
+        result.push([k,v]);
+      });
+      chai.expect(result).to.eql([["key1", 1], ["key2", 2], ["key3", 3]]);
+    });
+
+  });
+
+  describe("removing items", function() {
+
+    beforeEach(function() {
+      hash.addTail("key1", 1);
+      hash.addTail("key2", 2);
+      hash.addTail("key3", 3);
+    });
+
+    it("removes a particular key", function() {
+      hash.remove("key3");
+      chai.expect(hash.head.value).to.equal(1);
+      chai.expect(hash.tail.value).to.equal(2);
+    });
+
+    it("removes head", function() {
+      hash.removeHead();
+      chai.expect(hash.head.value).to.equal(2);
+      chai.expect(hash.tail.value).to.equal(3);
+    });
+
+    it("removes tail", function() {
+      hash.removeTail();
+      chai.expect(hash.head.value).to.equal(1);
+      chai.expect(hash.tail.value).to.equal(2);
+    });
+
+  });
+
+
+
+});
