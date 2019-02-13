b1aa621a1c06ee4ac89c39d39142a9359c9cc088
Roman Snitko <roman.snitko@gmail.com>
2019-02-13 20:59:58 +0700

    Fix: logmaster shouldn't throw text message errors in test env

--- test_server.js
+++ test_server.js
@@ -18,7 +18,7 @@ app.get("/", function (req, res) {
   });
 })
 
-app.post("/report_error", function (req, res, next) {
+app.post("/report_webface_error", function (req, res, next) {
   res.type("text/plain");
   res.end("OK");
 });
